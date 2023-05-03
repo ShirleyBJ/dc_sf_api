0c64acc0a42c37946800d1028b737898e9898752
Shirley BERCHEL <shirleyberchel@gmail.com>
2023-05-03 09:23:28 +0200

    login et register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -29,32 +29,18 @@ class RegistrationController extends AbstractController
         if($isValid !== true){
             return new JsonResponse($isValid, 400);
         }
-        return new JsonResponse('User valide', 200);
 
+        // encode the plain password
+        $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                $user,
+                $user->getPlainPassword()
+            )
+        );
 
-        
+        $entityManager->persist($user);
+        $entityManager->flush();
 
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('app_category');
-        }
-
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        return new JsonResponse('Inscription effectué !', 200);
     }
 }
